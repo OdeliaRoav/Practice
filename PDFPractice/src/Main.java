2b08c82b2ee94193594185b9fe2609e435d59456
pmsoo <pmsoo4@naver.com>
2025-01-12 23:38:55 +0900

    Initial

--- PDFPractice/src/Main.java
+++ PDFPractice/src/Main.java
@@ -1,9 +1,29 @@
 
+import java.util.Scanner;
+import java.util.Stack;
+
+
+class stack {
+	String value;
+	
+	public stack(String value) {
+		this.value = value;
+	}
+	
+	
+}
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer>stack = new Stack<>();
+		
+		
+		
+		
+		
+		
 	}
 
 }
